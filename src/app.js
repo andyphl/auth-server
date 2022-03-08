e9a9e7af192bb9d84f1f3371b9ff0fbfe6e4941f
andyphl <phlai951016@gmail.com>
2022-03-08 22:33:36 +0800

    Add refresh-access token based auth

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const routes = require("./routes/routes");
+const { sequelize } = require("./models");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_HOST,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", routes);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, async () => {
   console.log(`Server listening on port ${process.env.PORT || 5000}`);
+
+  // load up database
+  await sequelize.authenticate();
 });
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,4 +5,7 @@ routes.get("/healthcheck", (req, res, next) => {
   res.sendStatus(200);
 });
 
+const authRoutes = require("./auth.routes");
+routes.use("/auth", authRoutes);
+
 module.exports = routes;
